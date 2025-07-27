4bb7535ad3f8c829c0a2c26377d30f36696fb63e
Zunaid Sheikh <lamhaa786@gmail.com>
2025-07-27 23:29:58 +0545

    middleware

--- src/routes/categoryRoute.ts
+++ src/routes/categoryRoute.ts
@@ -1,12 +1,13 @@
 import express, { Router } from "express"
 import CategoryController from "../controllers/categoryController"
+import Middleware from "../middleware/middleware"
 
 const router:Router=express.Router()
 
-router.route("/category").post(CategoryController.addCategory)
+router.route("/category").post(Middleware.isLoggedI,CategoryController.addCategory)
 router.route("/category").get(CategoryController.fetchAllCategories)
 router.route("/category/:id").get(CategoryController.fetchSingleCategory)
-router.route("/category/:id").delete(CategoryController.deleteCategory)
-router.route("/category/:id").patch(CategoryController.editCategory)
+router.route("/category/:id").delete(Middleware.isLoggedI,CategoryController.deleteCategory)
+router.route("/category/:id").patch(Middleware.isLoggedI,CategoryController.editCategory)
 
 export default router
--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -1,12 +1,13 @@
 import express, { Router } from "express"
 import ProductController from "../controllers/productController"
+import Middleware from "../middleware/middleware"
 
 const router:Router=express.Router()
 
-router.route("/product").post(ProductController.createProduct)
-router.route("/product/:id").patch(ProductController.updateProduct)
+router.route("/product").post(Middleware.isLoggedI,ProductController.createProduct)
+router.route("/product/:id").patch(Middleware.isLoggedI,ProductController.updateProduct)
 router.route("/product").get(ProductController.getAllProducts)
 router.route("/product/:id").get(ProductController.getSingleProduct)
-router.route("/product/:id").delete(ProductController.deleteProduct)
+router.route("/product/:id").delete(Middleware.isLoggedI,ProductController.deleteProduct)
 
 export default router
